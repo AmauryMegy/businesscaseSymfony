2c2d5e1400c8d6ac1a881568cab56c7ba4855d28
Amaury Megy <megy.amaury@gmail.com>
2022-07-28 16:46:14 +0200

    Add API configuration

--- src/Entity/PaymentMethod.php
+++ src/Entity/PaymentMethod.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PaymentMethodRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PaymentMethodRepository::class)]
+#[ApiResource()]
 class PaymentMethod
 {
     #[ORM\Id]
